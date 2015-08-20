6a318e03f909b5df466e8b1f11c7e48d251963da
whimtime <tiziano.interlandi@gmail.com>
2015-08-20 21:18:20 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## this function cache the value of inverse matrix
+makeCacheMatrix <- function(x = matrix()) 
+{
+  # Initialize global value to NULL
+  cacheValue <- NULL
+  
+  # create the set function with the <<- operator to assign value to cacheValue (different environment)
+  set <- function(y) {
+    x <<- y
+    cacheValue <<- NULL
+  }
+  
+  # create the get function 
+  get <- function() x
+  # create the setMatrix function to set the value to chached matrix
+  setMatrix <- function(inverse) cacheValue <<- inverse
+  # get the inverted matrix from cache
+  getInverse <- function() cacheValue
+  
+  # return the created functions to the working environment
+  list(set = set, get = get, setMatrix = setMatrix,getInverse = getInverse)
+    
 }
 
+# this function calculate the inverse of matrix
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+  ## assign of the inverse matrix in the global variable
+  cacheValue <- x$getInverse()
+  
+  # check if the cached value exists (singleton like), if exists return the global variable
+  #the function break
+  if (!is.null(cacheValue)) {
+    return(cacheValue)
+  }
+  
+  # create matrix since it does not exist
+  matrix <- x$get()
+ 
+    # set and return inverse of matrix
+    cacheValue <- solve(matrix, ...)
+    #set the value to the cached variable
+    x$setMatrix(cacheValue)
+   
+  return (cacheValue)
 }
